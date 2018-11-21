42bc05127082d1720206fefc4c12cf83246f9b63
xindubawukong <xindubawukong@outlook.com>
2018-11-21 17:22:37 +0800

    maybe right

--- sources_1/imports/new/MMU.v
+++ sources_1/imports/new/MMU.v
@@ -16,10 +16,21 @@ module MMU(
 	output reg [31:0] storeData_o,
 	output reg [19:0] instAddr_o,
 	output reg [19:0] dataAddr_o,
-	output reg[15:0] led_o,
-	output reg[7:0] dpy0_o,
-	output reg[7:0] dpy1_o
+	output reg [15:0] led_o,
+	output reg [7:0] dpy0_o,
+	output reg [7:0] dpy1_o
 );
+
+    wire [7:0] seg0_o;
+    wire [7:0] seg1_o;
+    SEG7_LUT seg0(
+        .iDIG(storeData_i[3:0]),
+        .oSEG1(seg0_o)
+    );
+    SEG7_LUT seg1(
+        .iDIG(storeData_i[7:4]),
+        .oSEG1(seg1_o)
+    );
 	always @(*) begin 
 		if(rst == 1'b1) begin
 			ramOp_o <= `MEM_NOP;
@@ -34,19 +45,23 @@ module MMU(
 		end
 		else if (data_ramAddr_i == 32'hbfd00400) begin
 		    ramOp_o <= ramOp_i;
-		    load_data_o <= load_data_i;
+            load_data_o <= load_data_i;
             load_inst_o <= load_inst_i;
-                        
             storeData_o <= storeData_i;
             instAddr_o <= inst_ramAddr_i[21:2];
             dataAddr_o <= data_ramAddr_i[21:2];
-		    led_o <= storeData_i[15:0];
+            led_o <= storeData_i[15:0];
 		end
-		/*else if (data_ramAddr_i == 32'hbfd00408) begin
-		    ramOp_o <= `MEM_NOP;
-		    dpy0_o <= storeData_i[7:0];
-		    dpy1_o <= storeData_i[15:8];
-		end*/
+		else if (data_ramAddr_i == 32'hbfd00408) begin
+		    ramOp_o <= ramOp_i;
+            load_data_o <= load_data_i;
+            load_inst_o <= load_inst_i;
+            storeData_o <= storeData_i;
+            instAddr_o <= inst_ramAddr_i[21:2];
+            dataAddr_o <= data_ramAddr_i[21:2];
+		    dpy0_o <= seg0_o;
+		    dpy1_o <= seg1_o;
+		end
 		else begin
 			ramOp_o <= ramOp_i;
 			load_data_o <= load_data_i;
